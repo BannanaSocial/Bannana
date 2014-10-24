e6a3df08e98efd9f308a8f7f5a5f33ca6f72391e
jlogzz <juan_lorenzo_@hotmail.com>
2014-10-24 18:28:50 -0500

    Init

---
+++ app/config/database.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Configuracion de la base de datos
+ */
+R::setup('mysql:host=localhost;dbname=template', // host|ip; nombre de la base de datos
+         'template', //user
+         'password'); //password
+         //mysql
