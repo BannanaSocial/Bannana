8ef07b894f035994d2d64046d0b5b940a2b56252
jlogzz <juan_lorenzo_@hotmail.com>
2014-10-25 07:03:47 -0500

    Fix de fb

--- app/controllers/dashboard.controller.php
+++ app/controllers/dashboard.controller.php
@@ -29,55 +29,75 @@ $app->get('/dashboard/', $authenticate($app, 'admin'), function() use ($app){
 	$fanpage = R::findOne('fanpage',' user = :param ',
 	           array(':param' => $user->id )
 	         );
+	if($fanpage){
+		$inbox = json_decode(file_get_contents("https://graph.facebook.com/".$fanpage->fbid."/conversations?access_token=".$fanpage->fbtoken));
 
-	$inbox = json_decode(file_get_contents("https://graph.facebook.com/".$fanpage->fbid."/conversations?access_token=".$fanpage->fbtoken));
+		foreach ($inbox->data[0]->messages->data as $message) {
 
-	foreach ($inbox->data[0]->messages->data as $message) {
+			$fbmessage = R::findOne('fbmessage',' fbid = :param ',
+			           array(':param' => $message->id )
+			         );
 
-		$fbmessage = R::findOne('fbmessage',' fbid = :param ',
-		           array(':param' => $message->id )
+			if($fbmessage){
+
+			}else{
+
+				$fbmessage = R::dispense('fbmessage');
+
+				$fbmessage->fbid = $message->id;
+				$fbmessage->from = $message->from->name;
+				$fbmessage->fromid = $message->from->id;
+				$fbmessage->to = $message->to->data[0]->name;
+				$fbmessage->toid = $message->to->data[0]->id;
+				$fbmessage->message = $message->message;
+				$fbmessage->status = "unread";
+
+				if($fbmessage->fromid == $fanpage->fbid){
+					$ticket = R::findOne('ticket',' userfbid = :param ',
+				           array(':param' => $fbmessage->toid )
+				         );
+					$userfbid = $fbmessage->toid;
+				}else{
+					$ticket = R::findOne('ticket',' userfbid = :param ',
+				           array(':param' => $fbmessage->fromid )
+				         );
+					$userfbid = $fbmessage->fromid;
+				}
+
+				if($ticket){
+					$ticket->status = "pending";
+				}else{
+					$ticket = R::dispense('ticket');
+					$ticket->type = "fbmessage";
+					$ticket->status = "pending";
+					$ticket->department = 1;
+					$ticket->userfbid = $userfbid;
+					$ticket->fanpage = $fanpage->id;
+				}
+
+				R::store($ticket);
+
+				$fbmessage->ticket = $ticket->id;
+				R::store($fbmessage);
+			}
+		}
+
+		$inbox = array();
+
+		$tickets = R::find('ticket',' fanpage = :param ',
+		           array(':param' => $fanpage->id )
 		         );
 
-		if($fbmessage){
-
-		}else{
-
-			$fbmessage = R::dispense('fbmessage');
-
-			$fbmessage->fbid = $message->id;
-			$fbmessage->from = $message->from->name;
-			$fbmessage->fromid = $message->from->id;
-			$fbmessage->to = $message->to->data[0]->name;
-			$fbmessage->toid = $message->to->data[0]->id;
-			$fbmessage->message = $message->message;
-			$fbmessage->status = "unread";
-
-			if($fbmessage->fromid == $fanpage->fbid){
-				$ticket = R::findOne('ticket',' userfbid = :param ',
-			           array(':param' => $fbmessage->toid )
+		foreach ($tickets as $ticket) {
+			if($ticket->type == "fbmessage"){
+				$fbmessages = R::find('fbmessage',' ticket = :param ',
+			           array(':param' => $ticket->id )
 			         );
-				$userfbid = $fbmessage->toid;
-			}else{
-				$ticket = R::findOne('ticket',' userfbid = :param ',
-			           array(':param' => $fbmessage->fromid )
-			         );
-				$userfbid = $fbmessage->fromid;
+
+				foreach ($fbmessages as $message) {
+					$inbox[] = $message;
+				}
 			}
-
-			if($ticket){
-				$ticket->status = "pending";
-			}else{
-				$ticket = R::dispense('ticket');
-				$ticket->type = "fbmessage";
-				$ticket->status = "pending";
-				$ticket->department = 1;
-				$ticket->userfbid = $userfbid;
-			}
-
-			R::store($ticket);
-
-			$fbmessage->ticket = $ticket->id;
-			R::store($fbmessage);
 		}
 	}
 
@@ -85,7 +105,8 @@ $app->get('/dashboard/', $authenticate($app, 'admin'), function() use ($app){
 				  'profilepic' => $profilepic,
 				  'name' => $user->name,
 				  'fanpages' => $fanpages,
-				  'allpages' => $allpages->data);
+				  'allpages' => $allpages->data,
+				  'messages' => $inbox);
 
     $app->render('dashboard.html.twig', $data);
 });
